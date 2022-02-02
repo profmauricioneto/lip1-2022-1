8365101e3a4a40fdd3c18ff0b7c47571316f5210
Mauricio Moreira Neto <mauricio.moreira@unichristus.edu.br>
2022-02-02 07:53:18 -0300

    Rename Package
    Change the package name for better adapt in the context.

--- JavaContents/src/class0/FirstCodeJava.java
+++ JavaContents/src/class0/FirstCodeJava.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aula0;
+package class0;
 
 // import Scanner Java class
 import java.util.Scanner;
