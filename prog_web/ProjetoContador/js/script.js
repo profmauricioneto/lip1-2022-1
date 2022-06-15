96579a3c3a51be0a7fc70733e0e90b5c9e5952de
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2022-06-15 07:21:36 -0300

    add: projeto contador

---
+++ prog_web/ProjetoContador/js/script.js
@@ -0,0 +1,40 @@
+// variables catch by DOM
+var number = document.getElementById("number");
+var increment = document.getElementById("btn-increment");
+var decrement = document.getElementById("btn-decrement");
+var reset = document.getElementById("btn-reset");
+let numberCounter = parseInt(number.innerHTML);
+
+// function to change the color of the number
+function changeNumberColor(numberCounter) {
+    var color = "";
+    if (numberCounter < 0) {
+        color = "red";
+    } else if (numberCounter > 0) {
+        color = "green";
+    } else {
+        color = "#34495E";
+    }
+    return color;
+}
+
+// function when click in reset button 
+reset.addEventListener("click", function () {
+    var n = number.innerHTML = 0;
+    number.style.color = changeNumberColor(n);
+});
+
+
+// function when click in increment button
+increment.addEventListener("click", function () {
+    numberCounter++;
+    number.innerHTML = numberCounter.toString();
+    number.style.color = changeNumberColor(numberCounter);
+});
+
+// function when click in decrement button
+decrement.addEventListener("click", function () {
+    numberCounter--;
+    number.innerHTML = numberCounter.toString();
+    number.style.color = changeNumberColor(numberCounter);
+});
